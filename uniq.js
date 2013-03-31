03298bc8083cad87b507e72a711575d41a56f069
Mikola Lysenko <mikolalysenko@gmail.com>
2013-03-30 21:50:55 -0500

    added sorting flag

--- uniq.js
+++ uniq.js
@@ -4,7 +4,6 @@ function unique_pred(list, compare) {
   var ptr = 1
     , len = list.length
     , a=list[0], b=list[0]
-  list.sort(compare)
   for(var i=1; i<len; ++i) {
     b = a
     a = list[i]
@@ -24,7 +23,6 @@ function unique_eq(list) {
   var ptr = 1
     , len = list.length
     , a=list[0], b = list[0]
-  list.sort()
   for(var i=1; i<len; ++i, b=a) {
     b = a
     a = list[i]
@@ -40,12 +38,18 @@ function unique_eq(list) {
   return list
 }
 
-function unique(list, compare) {
+function unique(list, compare, sorted) {
   if(list.length === 0) {
     return []
   }
   if(compare) {
+    if(!sorted) {
+      list.sort(compare)
+    }
     return unique_pred(list, compare)
+  }
+  if(!sorted) {
+    list.sort()
   }
   return unique_eq(list)
 }
