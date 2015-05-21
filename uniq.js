40c67e7a441043ad931407aa54666567297614b5
Gary Kenneally <garykenneally@gmail.com>
2015-05-21 20:14:28 +0100

    refactored unique functions into a single funtion and added more tests

--- uniq.js
+++ uniq.js
@@ -1,57 +1,38 @@
-"use strict"
-
-function unique_pred(list, compare) {
-  var ptr = 1
-    , len = list.length
-    , a=list[0], b=list[0]
-  for(var i=1; i<len; ++i) {
-    b = a
-    a = list[i]
-    if(compare(a, b)) {
-      if(i === ptr) {
-        ptr++
-        continue
-      }
-      list[ptr++] = a
-    }
-  }
-  list.length = ptr
-  return list
-}
-
-function unique_eq(list) {
-  var ptr = 1
-    , len = list.length
-    , a=list[0], b = list[0]
-  for(var i=1; i<len; ++i, b=a) {
-    b = a
-    a = list[i]
-    if(a !== b) {
-      if(i === ptr) {
-        ptr++
-        continue
-      }
-      list[ptr++] = a
-    }
-  }
-  list.length = ptr
-  return list
-}
+"use strict";
 
 function unique(list, compare, sorted) {
   if(list.length === 0) {
-    return list
+    return list;
   }
-  if(compare) {
+
+  if(!compare) {
+    compare = function(l, r){return l !== r; };
     if(!sorted) {
-      list.sort(compare)
+      list.sort();
     }
-    return unique_pred(list, compare)
   }
-  if(!sorted) {
-    list.sort()
+  else{
+    if(!sorted) {
+      list.sort(compare);
+    }
   }
-  return unique_eq(list)
+
+  var ptr = 1
+    , len = list.length
+    , a=list[0], b=list[0];
+  for(var i=1; i<len; ++i) {
+    b = a;
+    a = list[i];
+    if(compare(a, b)) {
+      if(i === ptr) {
+        ptr++;
+        continue;
+      }
+      list[ptr++] = a;
+    }
+  }
+  list.length = ptr;
+  return list;
 }
 
-module.exports = unique
+module.exports = unique;
