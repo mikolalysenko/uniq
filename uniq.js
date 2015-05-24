2f2df410805f011f535dcc125e2c8e10aff51b73
Gary Kenneally <garykenneally@gmail.com>
2015-05-24 22:37:55 +0100

    refactored to remove function comparison when no compare parameter is supplied

--- uniq.js
+++ uniq.js
@@ -1,38 +1,53 @@
 "use strict"
 
 function unique(list, compare, sorted) {
-  if(list.length === 0) {
+  if (list.length === 0) {
     return list
   }
 
-  if(!compare) {
-    compare = function(l, r){return l !== r }
-    if(!sorted) {
+  if (!compare) {
+    if (!sorted) {
       list.sort()
     }
-  }
-  else{
-    if(!sorted) {
+
+    var ptr = 1,
+      len = list.length,
+      a = list[0],
+      b = list[0]
+
+    for (var i = 1; i < len; ++i) {
+      b = a
+      a = list[i]
+      if (a === b) continue
+
+      if (i === ptr) {
+        ptr++
+        continue
+      }
+      list[ptr++] = a
+    }
+  } else {
+    if (!sorted) {
       list.sort(compare)
     }
-  }
 
-  var ptr = 1
-    , len = list.length
-    , a=list[0], b=list[0]
-  for(var i=1; i<len; ++i) {
-    b = a
-    a = list[i]
-    if(compare(a, b)) {
-      if(i === ptr) {
+    var ptr = 1,
+      len = list.length,
+      a = list[0],
+      b = list[0]
+
+    for (var i = 1; i < len; ++i) {
+      b = a
+      a = list[i]
+      if (!compare(a, b)) continue
+      if (i === ptr) {
         ptr++
         continue
       }
       list[ptr++] = a
     }
   }
-  list.length = ptr
-  return list
+  return list.slice(0, ptr)
 }
 
 module.exports = unique
