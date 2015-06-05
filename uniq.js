9fba66bc7a4e7591bde7b06e5a88985a17d5d20a
Manish Jethani <manish.jethani@gmail.com>
2015-06-05 12:38:27 +0530

    Remove redundant assignment

--- uniq.js
+++ uniq.js
@@ -22,8 +22,8 @@ function unique_pred(list, compare) {
 function unique_eq(list) {
   var ptr = 1
     , len = list.length
-    , a=list[0], b = list[0]
-  for(var i=1; i<len; ++i, b=a) {
+    , a=list[0], b=list[0]
+  for(var i=1; i<len; ++i) {
     b = a
     a = list[i]
     if(a !== b) {
