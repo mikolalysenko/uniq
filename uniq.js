6d26a3c70b6f6e16d73e4abac3053d57a7d0d89c
Guillaume Comte <guillaume.comte10@gmail.com>
2019-02-05 21:30:50 +0100

    Refactor unique_eq to use unique_pred

--- uniq.js
+++ uniq.js
@@ -20,22 +20,7 @@ function unique_pred(list, compare) {
 }
 
 function unique_eq(list) {
-  var ptr = 1
-    , len = list.length
-    , a = list[0], b = list[0]
-  for(var i=1; i<len; ++i) {
-    b = a
-    a = list[i]
-    if(a !== b) {
-      if(i === ptr) {
-        ptr++
-        continue
-      }
-      list[ptr++] = a
-    }
-  }
-  list.length = ptr
-  return list
+  return unique_pred(list, (a, b) => a !== b)
 }
 
 function unique(list, compare, sorted) {
